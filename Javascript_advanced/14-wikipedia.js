2e0068140350951c33701c9ba6f2daee1e281050
dacods <dacods.21@gmail.com>
2024-11-19 15:00:32 -0600

    Simple callback :(

--- Javascript_advanced/14-wikipedia.js
+++ Javascript_advanced/14-wikipedia.js
@@ -6,6 +6,18 @@ function createElement(data) {
 
 function queryWikipedia(callback) {
     let xhttp = new XMLHttpRequest();
-    let url = 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=Stack%20Overflow&origin=*'
+    xhttp.open('GET', 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=Stack%20Overflow&origin=*', true)
 
+
+    xhttp.onreadystatechange = function() {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            let response = JSON.parse(xhttp.responseText);
+            let page = response.query.pages;
+            let extract = page[Object.keys(page)[0]].extract;
+            callback(extract);
+        }
+    };
+    xhttp.send();
 }
+
+queryWikipedia(createElement);
